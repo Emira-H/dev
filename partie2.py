99716c3b6ad7001bfc3625a7be4b585df0a56a8b
Emira_Harrioui <harrioui.e@gmail.com>
2019-10-16 17:11:03 +0200

    Exercice 1 of partie 2

---
+++ partie2.py
@@ -0,0 +1,10 @@
+##Partie 2
+#Exercice 1: True ou False
+var1="Coucou Bruno"
+var2=""
+if len(var1)==0 and len(var2)==0:
+    print("True")
+else:
+    print("False")
+
+#Exercice 2: Calculer mon âge

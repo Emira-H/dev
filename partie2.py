7c4d76b3af1a67416f393f08f6b39102fda48175
Emira_Harrioui <harrioui.e@gmail.com>
2019-10-17 01:51:08 +0200

    Exercices of Partie 2

--- partie2.py
+++ partie2.py
@@ -8,3 +8,40 @@ else:
     print("False")
 
 #Exercice 2: Calculer mon âge
+
+year=int(input("quelle est l'année actuelle? "))
+birth_year=int(input("quelle est votre année de naissance? "))
+age=year-birth_year
+print(age)
+neighbour_age=int(input("quelle est l'age de votre voisin? "))
+print(40+neighbour_age)
+
+#Exercice 3: Problème de chaussures
+
+prix1=70
+prix2=59
+prix3=20
+total=(prix1+prix2+prix3)*0.8
+print(total)
+
+#Exercice 4: une calculatrice Python
+
+nombre1=float(input("Saisir un nombre "))
+nombre2=float(input("Saisir un deuxième nombre "))
+somme= nombre1+nombre2
+print(somme)
+
+#Exercice 5: travailler avec les propriétés
+
+prenom=input("Quel est votre prénom?" )
+nom=input("Quel est votre nom?" )
+PremLettre_prenom=prenom[0]
+DernLettre_prenom=prenom[-1]
+initial_prenom=PremLettre_prenom+DernLettre_prenom
+PremLettre_nom=nom[0]
+DernLettre_nom=nom[-1]
+initial_nom=PremLettre_nom+DernLettre_nom
+
+print(initial_prenom.upper())
+print(initial_nom.upper())
+print((initial_prenom+initial_nom).upper())

4bbca856544d2f56a132ee78e051c4987e395482
Emira_Harrioui <harrioui.e@gmail.com>
2019-10-27 23:13:59 +0100

    Add exo2 of partie 2 of intermediary exercices

---
+++ Exo_Inter/Partie2/Exo2.py
@@ -0,0 +1,17 @@
+
+# Exo 2
+
+print("Exercice 2: Matrix dans le terminal")
+
+
+
+def boucle():
+
+    nb_line = 20
+    i = 1
+    while i <= nb_line:
+        print(0)
+        i += 1
+print("------")
+
+boucle()

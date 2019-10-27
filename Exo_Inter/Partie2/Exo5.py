c32d016c988c093f391eacd67999c1232a218194
Emira_Harrioui <harrioui.e@gmail.com>
2019-10-27 23:19:31 +0100

    add exo5 of party 2

---
+++ Exo_Inter/Partie2/Exo5.py
@@ -0,0 +1,12 @@
+# Exercice 5
+print(" Exercice 5: les tirets ça compte!")
+
+
+def only_underscore(a):
+    if type(a) == str:
+        new_str = text.replace("-", "_")
+        print(new_str)
+    else:print("")
+
+
+only_underscore(text)
